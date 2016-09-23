908a1b119d328d797edefc1597322386d7968d92
Cyril Jouve <jv.cyril@gmail.com>
2016-09-23 16:27:23 +0200

    init w/ jinjagen

---
+++ jinjagen/jinjagen.py
@@ -0,0 +1,87 @@
+#!/usr/bin/env python
+
+import jinja2
+import json
+import yaml
+import logging
+import argparse
+import sys
+import os
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('template', 
+                        nargs='+',
+                        help='template file(s)')
+    parser.add_argument('-d', '--data',
+                        help='template data')
+    parser.add_argument('-o', '--out',
+                        help='specify output file, if set only 1 template is processed')
+    parser.add_argument('-i', '--inplace',
+                        action='store_true',
+                        help='inplace editing, if the file ext is .j2, then it is removed')
+    parser.add_argument('-V', '--verbose', 
+                        action='store_true',
+                        help='verbose mode, off by default')
+    args = parser.parse_args()
+
+    if args.verbose:
+        level = logging.DEBUG
+    else:
+        level = logging.INFO
+    logging.basicConfig(level=level)
+
+    if args.out and args.inplace:
+        logging.error('-i and -o are incompatible')
+        return 1
+
+    if len(args.template) > 1 and args.out:
+        logging.error('can\'t have more than 1 templace and -o')
+        return 1
+
+    if args.data == '-':
+        rawdata = sys.stdin.read()
+        ext = '.yaml'
+    else:
+        with open(args.data) as fl:
+            _, ext = os.path.splitext(args.data)
+            rawdata = fl.read()
+
+    if ext == '.yaml':
+        load = yaml.load
+    elif ext == '.json':
+        load = json.loads
+
+    data = load(rawdata)
+
+    for template in args.template:
+        head, tail = os.path.split(os.path.realpath(template))
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader(head))
+        tpl = env.get_template(tail)
+        out = tpl.render(data)
+
+        if args.inplace:
+            base, tplext = os.path.splitext(tail)
+            if tplext == '.j2':
+                outname = base
+            else:
+                outname = tail + '.new'
+            stdout = None
+        elif args.out:
+            outname = args.out
+            stdout = None
+        else:
+            stdout = sys.stdout
+
+        if stdout:
+            stdout.write(out)
+            stdout.flush()
+        else:
+            with open(outname, 'w') as fl:
+                fl.write(out)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
+
---
+++ jinjagen/test.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+rm -rf out
+mkdir out
+
+# test output to stdout (yaml)
+mkdir out/1
+./jinjagen.py -d test.yaml test1.txt.j2 > out/1/test1.txt
+
+# test output to stdout (json)
+mkdir out/2
+./jinjagen.py -d test.json test1.txt.j2 > out/2/test1.txt
+
+# test output to stdout (stdin)
+mkdir out/3
+./jinjagen.py -d - test1.txt.j2 < test.yaml > out/3/test1.txt
+
+# test output
+mkdir out/4
+./jinjagen.py -d test.yaml -o test/4/test1.txt test1.txt.j2
+
+# test inplace (.j2 ext)
+mkdir out/5
+./jinjagen.py -d test.yaml -i test1.txt.j2 && mv test1.txt out/5
+
+# test inclace (! .2 ext)
+mkdir out/6
+./jinjagen.py -d test.yaml -i test2.txt && mv test2.txt.new out/6
+
+# test inplace (multiple in)
+mkdir out/7
+./jinjagen.py -d test.yaml -i test3.txt test4.txt && mv test3.txt.new test4.txt.new out/7
+  
